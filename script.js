1f03ae99aa30126e6dcb602e5269afad63597b19
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2019-12-18 23:13:26 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-var A = 0,M,Q,NeM,count,QMO=0,QO,power;
-
+var A,M,Q,NeM,count,QMO=0,QO,power,re,initCount,neg,decResult,posResult,index;
+var table;
+//It takes input from the form and sends it to get processed
 function input(){
+    var i;
+    document.getElementById("submit").disabled = true;
+    for(i=1;i<=13;i++){
+        reset(i);
+        console.log("Shit");
+    }
     var a = document.getElementById("a").value;
     var b = document.getElementById("b").value;
     if(a.length == 0 || b.length == 0){
         window.alert("Fill the form");
         return 0;
     }
-    
+    if(a<0||b<0){
+        neg = 1;
+    }else{
+        neg = 0;
+    }
     M = convertDecimalToBinary(a);
     Q = convertDecimalToBinary(b);
     count = Q.toString().length+1;
-    power = Math.pow(10,(Q.toString().length - 1));
+    initCount = count;
+    power = Math.pow(10,count-1);
     NeM = twoComplement(M);
-    Proc(1,0);
+    Proc(0);
 }
 //Display Functions
 function Proc(i){
     var j;
     QO = Q%10;
-    if(count == 0){
+    //I is the step number ie. i = 1 is start and i = 13 is stop and between is the steps to run
+    if(i==13){
         return 0;
     }
+    //for reseting everything so that steps can be shown
     for(j=1;j<=13;j++){
         colorReturn(j);
-    }   
+    }
+    //when i = 7 it reaches first condition
     if(i==7){
+        //first condition for 10
         if(QO == 1 && QMO == 0){
             i = 8;
-            console.log(i);
+        //second condition for 01
         }else if(QO == 0 && QMO == 1){
             i = 9;
+        //final condition for 11 or 00
         }else{
             i = 10;
         }
+    //after condition after 10 and 01 is processed it has to go to a same point
     }else if(i==8||i==9){
+        //i = 10 is arithmetic shift position
         i = 10;
+    //i = 11 is condition of count = 0?
     }else if(i == 11){
         if(count!=0){
             i = 7;
@@ -43,13 +63,29 @@ function Proc(i){
             i++;
         }
     }else{
+        //increment for next value
         i++;
     }
+    //changing colour of that position
     colorChange(i);
+    //delay in changing colour and displaying so that we can observe
     setTimeout(Proc,500,i);
 }
 function colorChange(i){
     var change;
+    if(i>7 && i!=11 && i !=13){
+        table = document.getElementById("table");
+        var row = table.insertRow(index);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = A;
+        cell2.innerHTML = Q;
+        cell3.innerHTML = QMO;
+        cell4.innerHTML = count;
+        index++;
+    }
     switch(i){
         case 1:
             change = "start";
@@ -78,7 +114,7 @@ function colorChange(i){
             change = "ConditionO";
             document.getElementById(change).innerHTML = QO + "," + QMO;
             document.getElementById(change).style.marginLeft = "-12px";
-            document.getElementById("diamond").style.borderBottomColor = "yellow";
+            document.getElementById("diamond1").style.borderBottomColor = "yellow";
             document.getElementById("diamondBottom").style.borderTopColor = "yellow";
             break;
         case 8:
@@ -104,12 +140,22 @@ function colorChange(i){
             break;
         case 12:
             change = "result";
+            re = parseInt(A+""+Q);
+            if(neg==1){
+                posResult = twoComplement(re);
+            }else{
+                posResult = re;
+            }
+            decResult = convertBinaryToDecimal(posResult);
+            document.getElementById(change).innerHTML = re+"<br>"+decResult;
             break;
         case 13:
             change = "stop";
+            document.getElementById("submit").disabled = false;
             break;
                                 
     }
+    //changing element so that we can observe
     document.getElementById(change).style.color = "red";
     document.getElementById(change).style.backgroundColor = "yellow";
 }
@@ -136,7 +182,7 @@ function colorReturn(i){
             break;
         case 7:
             change = "ConditionO";
-            document.getElementById("diamond").style.borderBottomColor = "grey";
+            document.getElementById("diamond1").style.borderBottomColor = "grey";
             document.getElementById("diamondBottom").style.borderTopColor = "grey";
             break;
         case 8:
@@ -158,18 +204,20 @@ function colorReturn(i){
             change = "stop";
             break;                  
     }
+    //reseting element back to normal
     document.getElementById(change).style.color = "black";
     document.getElementById(change).style.backgroundColor = "grey";
 }
 function convertDecimalToBinary(n)
 {
         var binaryNumber = 0;
-        var remainder = 0, i = 1,neg;
+        var remainder = 0, i = 1,ne;
+        //checking if number is negative
         if(n<0){
-            neg = 1;
+            ne = 1;
             n = Math.abs(n);
         }else{
-            neg = 0;
+            ne = 0;
         }
         while (n>=1)
         {	
@@ -178,7 +226,7 @@ function convertDecimalToBinary(n)
             binaryNumber = binaryNumber + (remainder*i);
             i = i*10;
         }
-        if(neg == 1){
+        if(ne == 1){
             return (twoComplement(binaryNumber));
         }else{
             return binaryNumber;
@@ -187,14 +235,21 @@ function convertDecimalToBinary(n)
 
 function convertBinaryToDecimal(n)
 {
-        var decimalNumber = 0, i = 0, remainder;
-        n=parseInt(n);
-        while (n!=0)
+        var decimalNumber = 0, i = 0, remainder, powTwo;
+        console.log("Fucking hell");
+        if(neg == 1){
+            n = n%power;
+        }
+        while (n>=1)
         {
             remainder = n%10;
-            n /= 10;
-            decimalNumber += remainder*pow(2,i);
+            n = parseInt(n/10);
+            powTwo = Math.pow(2,i);
+            decimalNumber = decimalNumber + (remainder*powTwo);
             ++i;
+        }
+        if(neg==1){
+            decimalNumber = decimalNumber * (-1);
         }
     return decimalNumber;
 }
@@ -263,7 +318,6 @@ function sub(){
         }
         i *=10;
     }
-    temp = temp + i * c; 
     return temp;
 }
 function add(){
@@ -301,15 +355,93 @@ function add(){
         }
         i *=10;
     }
-    temp = temp + i * c; 
     return temp;
 }
 function arithmeticShiftRight(){
     var temp1, temp2, temp3;
     temp1 = A % 10;
     temp2 = Q % 10;
-    temp3 = A % (power/10);
+    temp3 = parseInt(A / power);
     QMO = temp2;
     Q = parseInt(Q / 10) + (power * temp1);
     A = parseInt(A / 10) + (power * temp3);
+}
+function reset(i){
+    A=M=Q=NeM=count=QMO=QO=power=re=initCount=neg=decResult=posResult=0;
+    index=1;
+    var change;
+    switch(i){
+        case 1:
+            change = "start";
+            break;
+        case 2:
+            change = "InitValA";
+            document.getElementById(change).innerHTML= "A = 0000" ;
+            break;
+        case 3:
+            change = "InitValQ";
+            document.getElementById(change).innerHTML= "Q = NA";
+            break;
+        case 4:
+            change = "InitValQM";
+            document.getElementById(change).innerHTML= "Q(-1) = 0";
+            break;
+        case 5:
+            change = "InitValM";
+            document.getElementById(change).innerHTML = "M = NA";
+            break;
+        case 6:
+            change = "InitValC";
+            document.getElementById(change).innerHTML = "Count = n";
+            break;
+        case 7:
+            change = "ConditionO";
+            document.getElementById(change).innerHTML = "Q(O)Q(-1)";
+            document.getElementById(change).style.marginLeft = "-33px";
+            document.getElementById("diamond1").style.borderBottomColor = "grey";
+            document.getElementById("diamondBottom").style.borderTopColor = "grey";
+            break;
+        case 8:
+            change = "sub";
+            A = sub();
+            document.getElementById(change).innerHTML= "A = A - M";
+            break;
+        case 9:
+            change = "add";
+            A = add();
+            document.getElementById(change).innerHTML = "A = A + M";
+            break;
+        case 10:
+            change = "shift";
+            arithmeticShiftRight();
+            document.getElementById(change).innerHTML = "A , Q, QO";
+            document.getElementById("counter").innerHTML = "Count = Count - 1"
+            count--;
+            break;
+        case 11:
+            change = "Condition1";
+            document.getElementById(change).innerHTML = "Count = 0?"
+            break;
+        case 12:
+            change = "result";
+            document.getElementById(change).innerHTML = "Result";
+            break;
+        case 13:
+            change = "stop";
+            table = document.getElementById("table");
+            table.innerHTML = "";
+            var row = table.insertRow(0);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            cell1.innerHTML = "A";
+            cell2.innerHTML = "Q";
+            cell3.innerHTML = "Q(-1)";
+            cell4.innerHTML = "count";
+            break;                      
+    }
+    //changing element so that we can observe
+    document.getElementById(change).style.color = "black";
+    document.getElementById(change).style.backgroundColor = "grey";
 }

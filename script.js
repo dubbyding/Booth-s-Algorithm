9920e6b7bc6e1f1e47d066992a5fa3b1713bb620
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-01-23 11:56:00 +0545

    All Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var M, Q, A, QMO, NM, Final, position = 0;
+var M, Q, A, QMO, NM, Final, i = 0,count,l,index=1;
 //It takes input from the form and sends it to get processed
 function input() {
     var a = parseInt(document.getElementById("a").value);
@@ -8,48 +8,135 @@ function input() {
     M = convertDecimalToBinary(a);
     Q = convertDecimalToBinary(b);
     NM = twoComplement(M);
-    var l = Q.length,
-        count = l;
+    l = Q.length;
+    count = l;
     while (A.length != M.length) {
         A = A + "0";
     }
-    while (count != 0) {
-        if (Q[l - 1] == "0" && QMO == "1") {
-            A = add(A, M);
-        } else if (Q[l - 1] == "1" && QMO == "0") {
-            A = add(A, NM);
-        }
-        A = A.split("");
-        Q = Q.split("");
-        QMO = QMO.split("");
-        arithmeticShiftRight(A, Q, QMO);
-        A = A.join("");
-        Q = Q.join("");
-        QMO = QMO.join("");
-        count--;
-    }
-    Final = convertBinaryToDecimal(A + Q);
-    alert(Final);
+    document.getElementById("submit").disabled = true;
+    document.getElementById("next").disabled = false;
+    resetTable();
 }
-
-function run() {
-    while (count != 0) {
-        if (Q[l - 1] == "0" && QMO == "1") {
+function goNext(){
+    console.log(i);
+    
+    switch(i){
+        case 0:
+            document.getElementById("stop").style.color="black";
+            document.getElementById("start").style.color="Yellow";
+            i++;
+            break;
+        case 1:
+            document.getElementById("start").style.color="black";
+            document.getElementById("InitValA").innerHTML="A = " + A;
+            document.getElementById("InitValA").style.color="Yellow";
+            
+            i++;
+            break;
+        case 2:
+            document.getElementById("InitValA").style.color="black";
+            document.getElementById("InitValQ").innerHTML = "Q = " + Q;
+            document.getElementById("InitValQ").style.color="Yellow";
+            i++;
+            break;
+        case 3:
+            document.getElementById("InitValQ").style.color="black";
+            document.getElementById("InitValQM").innerHTML="Q(-1) = " + QMO;
+            document.getElementById("InitValQM").style.color="Yellow";
+            i++;
+            break;
+        case 4:
+            document.getElementById("InitValQM").style.color="black";
+            document.getElementById("InitValM").innerHTML="M = " + M;
+            document.getElementById("InitValM").style.color="Yellow";
+            i++;
+            break;
+        case 5:
+            document.getElementById("InitValM").style.color="black";
+            document.getElementById("InitValC").innerHTML="Count = " + count;
+            document.getElementById("InitValC").style.color="Yellow";
+            i++;
+            break;
+        case 6:
+            document.getElementById("InitValC").style.color="black";
+            document.getElementById("Condition1").style.color="black";
+            document.getElementById("ConditionO").innerHTML=Q[l-1]+QMO;
+            document.getElementById("ConditionO").style.color="Yellow";
+            if (Q[l - 1] == "0" && QMO == "1") {
+                i++;
+            } else if (Q[l - 1] == "1" && QMO == "0") {
+                i+=2;
+            }else{
+                i = 9;
+            }
+            insTable("Q(0)Q(-1) check");
+            break;
+        case 7:
             A = add(A, M);
-        } else if (Q[l - 1] == "1" && QMO == "0") {
+            document.getElementById("ConditionO").style.color="black";
+            document.getElementById("add").innerHTML ="A = "  + A;
+            document.getElementById("add").style.color="Yellow";
+            i+=2;
+            insTable("A = A + M");
+            break;
+        case 8:
             A = add(A, NM);
-        }
-        A = A.split("");
-        Q = Q.split("");
-        QMO = QMO.split("");
-        arithmeticShiftRight(A, Q, QMO);
-        A = A.join("");
-        Q = Q.join("");
-        QMO = QMO.join("");
-        count--;
+            document.getElementById("ConditionO").style.color="black";
+            document.getElementById("sub").innerHTML = "A = "+ A;
+            document.getElementById("sub").style.color="Yellow";
+            i++;
+            insTable("A = A - M");
+            break;
+        case 9:
+            A = A.split("");
+            Q = Q.split("");
+            QMO = QMO.split("");
+            arithmeticShiftRight(A, Q, QMO);
+            A = A.join("");
+            Q = Q.join("");
+            QMO = QMO.join("");
+            document.getElementById("add").style.color="black";
+            document.getElementById("sub").style.color="black";
+            document.getElementById("ConditionO").style.color="black";
+            document.getElementById("shift").innerHTML=A+","+Q+","+QMO;
+            document.getElementById("shift").style.color="Yellow";
+            i++;
+            insTable("Arithmetic Right Shift");
+            break;
+        case 10:
+            count--;
+            document.getElementById("shift").style.color="black";
+            document.getElementById("counter").innerHTML="Count = " + count;
+            document.getElementById("counter").style.color="Yellow";
+            i++;
+            insTable("Count--");
+            break;
+        case 11:
+            document.getElementById("counter").style.color="black";
+            document.getElementById("Condition1").style.color="Yellow";
+            if(count == 0){
+                i++;
+            }else{
+                i = 6; 
+            }
+            break;
+        case 12:
+            Final = convertBinaryToDecimal(A + Q);
+            document.getElementById("Condition1").style.color="black";
+            document.getElementById("result").innerHTML = "Result = " + A+""+Q+"<br>"+Final;
+            document.getElementById("result").style.color="Yellow";
+            i++;
+            break;
+        case 13:
+            document.getElementById("result").style.color="black";
+            document.getElementById("stop").style.color="Yellow";
+            alert(Final);
+            document.getElementById("submit").disabled = false;
+            document.getElementById("next").disabled = true;
+            i=0;
+            break;
     }
 }
-
 function convertDecimalToBinary(n) {
     var binaryNumber = new String;
     var remainder = 0,
@@ -200,4 +287,35 @@ function arithmeticShiftRight(A, Q, QMO) {
     for (i = l; i > 0; i--) {
         A[i] = A[i - 1];
     }
+}
+function insTable(reason){
+    table = document.getElementById("table");
+    var row = table.insertRow(index);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = A;
+    cell2.innerHTML = Q;
+    cell3.innerHTML = QMO;
+    cell4.innerHTML = count;
+    cell5.innerHTML = reason;
+    index++;
+}
+function resetTable(){
+    table = document.getElementById("table");
+    table.innerHTML = "";
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = "A";
+    cell2.innerHTML = "Q";
+    cell3.innerHTML = "Q(-1)";
+    cell4.innerHTML = "count";
+    cell5.innerHTML = "Reason";
+    index=1;
 }
